31b7924720399e8c87c939e97f03d074d8e350db
developer-munir <devolopermunir@gmail.com>
2022-08-22 17:39:15 +0600

    selected best 5 calculation

--- js/budget.js
+++ js/budget.js
@@ -1,9 +1,10 @@
 // calculate per player-budget
 document.getElementById("calculate-btn").addEventListener("click", function () {
   const playerBudgetField = getPlayerBudget("player-budget-field");
-  const totalPlayerCost = playerBudgetField * 5;
+  const totalPlayerCost = playerBudgetField * liContainer.childElementCount;
   setTotal("player-expenses", totalPlayerCost);
 });
+
 // calculation total budget
 document
   .getElementById("calculate-total-btn")
@@ -11,7 +12,7 @@ document
     const playerBudgetField = getPlayerBudget("player-budget-field");
     const managerBudgetField = getPlayerBudget("manager-budget");
     const coachBudgetField = getPlayerBudget("coach-budget");
-    const totalPlayerCost = playerBudgetField * 5;
+    const totalPlayerCost = playerBudgetField * liContainer.childElementCount;
     const totalBudget = managerBudgetField + coachBudgetField + totalPlayerCost;
     setTotal("all-budget", totalBudget);
   });
--- js/selected-v.js
+++ js/selected-v.js
@@ -1,19 +1,21 @@
-// 
 const liContainer = document.getElementById("li-container");
-
-
-
-
+const arr = [];
 const buttons = document.getElementsByClassName("selected-btn");
 for (const button of buttons) {
   button.addEventListener("click", function (e) {
+    e.stopImmediatePropagation;
     const playerNames = e.target.parentNode.children[0];
     const playerName = playerNames.innerText;
-    const li = document.createElement('li');
-    li.innerText = playerName;
-    liContainer.appendChild(li);
-    button.setAttribute('disabled',true);
-    console.log(playerName);
-
+    arr.push(playerName);
+    if (arr.length <= 5) {
+      const li = document.createElement("li");
+      li.innerText = playerName;
+      liContainer.appendChild(li);
+      button.setAttribute("disabled", true);
+    } else {
+      alert("do not add more than 5 players at a time");
+    }
   });
 }
+
+
